8cbb280ed34270824da33e2ec7aa04aeb6f01128
majid asgari <majid.asgari@snapp.cab>
2024-08-25 16:54:15 +0330

    replace ILogger with MyLogger

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	d2.IProvide(a).IProvide(l2)
 	si := d2.IInvoke(a.Name()).(*service.A)
 	si.Print(1024)
-	li := d2.IInvoke(l2.Name()).(*service.MyLogger)
+	li := d2.IInvoke(l2.Name()).(service.ILogger)
 	li.Info("info message")
 	li.Debug("debug message")
 	li.Error("error message")
