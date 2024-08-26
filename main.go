920a266c79a95b897886e122444cd68e5559cbf3
majid asgari <majid.asgari@snapp.cab>
2024-08-26 10:24:07 +0330

    define type alias for service.A

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"test/service"
 )
 
+type serviceA = service.A
+
 func main() {
 	//-------------Generic DI
 	d := di.GNew()
-	d.Provide(new(service.A)).Provide(service.NewLogger())
-	sa := di.Invoke[service.A](d)
+	d.Provide(new(serviceA)).Provide(service.NewLogger())
+	sa := di.Invoke[serviceA](d)
 	sa.Print(1234)
 
 	l := di.Invoke[service.ConcreteLogger](d)
@@ -19,10 +21,10 @@ func main() {
 	//-------------Interface DI
 	fmt.Print("------------ di with interface ------------ \n")
 	d2 := di.INew()
-	a := new(service.A)
+	a := new(serviceA)
 	l2 := service.NewLogger()
 	d2.IProvide(a).IProvide(l2)
-	si := d2.IInvoke(a.Name()).(*service.A)
+	si := d2.IInvoke(a.Name()).(*serviceA)
 	si.Print(1024)
 	li := d2.IInvoke(l2.Name()).(service.ILogger)
 	li.Info("info message")
